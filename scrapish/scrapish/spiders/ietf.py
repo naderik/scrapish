88548cd5d8bfa47cb8e6f7d2fcf83ed7d09bd425
Kasra Naderi <naderi.kasra@gmail.com>
2021-02-11 15:31:06 -0500

    Create the ietf spider

---
+++ scrapish/scrapish/spiders/ietf.py
@@ -0,0 +1,11 @@
+import scrapy
+
+
+class IetfSpider(scrapy.Spider):
+    name = 'ietf'
+    allowed_domains = ['pythonscraping.com']
+    start_urls = ['http://pythonscraping.com/linkedin/ietf.html']
+
+    def parse(self, response):
+        title = response.xpath('//span[@class="title"]/text()').get()
+        return {"title": title}
