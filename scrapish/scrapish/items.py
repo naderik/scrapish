59dc4bbb4e034c1ef35012cf8f710931ef2bfa34
Kasra Naderi <naderi.kasra@gmail.com>
2021-02-14 17:33:40 -0500

    create Article items

--- scrapish/scrapish/items.py
+++ scrapish/scrapish/items.py
@@ -1,12 +1,7 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class ScrapishItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Article(scrapy.Item):
+    title = scrapy.Field()
+    url = scrapy.Field()
+    lastUpdated = scrapy.Field()
--- scrapish/scrapish/spiders/wikipedia.py
+++ scrapish/scrapish/spiders/wikipedia.py
@@ -1,6 +1,7 @@
 import scrapy
-from scrapy .spiders import CrawlSpider, Rule
-from scrapy.linkextractors import  LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapish.items import Article
 
 
 class WikipediaSpider(CrawlSpider):
@@ -8,12 +9,14 @@ class WikipediaSpider(CrawlSpider):
     allowed_domains = ['en.wikipedia.org']
     start_urls = ['http://en.wikipedia.org/wiki/Kevin_Bacon']
 
-    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)]
+    rules = [
+        Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)
+    ]
 
-    def parse(self, response):
-        return {
-            'title': response.xpath('//h1/text()').get() or response.xpath('//h1/i/text()').get(),
-            'url': response.url,
-            'last-edited': response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
+    def parse_info(self, response):
+        article = Article()
+        article['title'] = response.xpath('//h1/text()').get() or response.xpath('//h1/i/text()').get()
+        article['url'] = response.url
+        article['last_edited'] = response.xpath('//li[@id="footer-info-lastmod"]/text()').get()
 
-        }
+        return article
